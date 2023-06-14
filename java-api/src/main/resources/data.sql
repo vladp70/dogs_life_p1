30e3eb3cc518b92233da965cd6784554c53435c7
Selvyn <swright@celestial.co.uk>
2023-06-14 18:34:18 +0100

    Blank Springboot Dogs Application

--- java-api/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
--- java-api/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS dogs;
-  
-CREATE TABLE dogs (
-    dog_id INT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    age INT NOT NULL
-);
-
