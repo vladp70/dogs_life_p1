2b2958ef2c86dbd369032fce594e2fb21ce9f3a2
Selvyn <swright@celestial.co.uk>
2023-06-15 20:03:01 +0100

    Solution files added to reference branch

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,21 +1,8 @@
-DROP TABLE IF EXISTS owners;
-
-CREATE TABLE owners (
-    owner_id INT NOT NULL primary key,
-    name VARCHAR(250) NOT NULL
-);
-
 DROP TABLE IF EXISTS dogs;
-  
+
 CREATE TABLE dogs (
     dog_id INT NOT NULL,
     name VARCHAR(250) NOT NULL,
-    age INT NOT NULL,
-    owner_id INT NOT NULL,
-    FOREIGN key
-        (owner_id) REFERENCES owners (owner_id)
+    age INT NOT NULL
 );
 
-create view dogs_n_owners as
-select d.dog_id as id, o.name as dog_owner, d.name as dog
-from dogs as d inner join owners as o on d.owner_id = o.owner_id;
