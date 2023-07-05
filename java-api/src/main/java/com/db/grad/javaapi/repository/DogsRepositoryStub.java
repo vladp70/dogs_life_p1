9b8a34aa55522004db56cd5f7aa61d65f7bdfa3a
Selvyn <swright@celestial.co.uk>
2023-07-05 22:30:43 +0100

    starting point for TDD session

--- java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepositoryStub.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepositoryStub.java
@@ -36,7 +36,6 @@ public class DogsRepositoryStub implements DogsRepository {
         for( Dog theDog: itsDogs)
             if( theDog.getName().equalsIgnoreCase(aDog.getName()) ) {
                 result.add(theDog);
-                break;
             }
 
         return result;
@@ -51,6 +50,7 @@ public class DogsRepositoryStub implements DogsRepository {
             if( theDog.getId() == aDog.getId()) {
                 retrievedDog = theDog;
                 retrievedDog.setName( aDog.getName() );
+                result = retrievedDog.getId();
                 break;
             }
         if( retrievedDog == null )
