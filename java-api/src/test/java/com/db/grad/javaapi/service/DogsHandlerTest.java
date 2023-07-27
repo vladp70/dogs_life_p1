cc07e0f57bb3ec6424b6b6a962af69b43bf93dda
BadulescuCatalin <badulescucatalin01@gmail.com>
2023-07-27 15:40:16 +0100

    task1

---
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -0,0 +1,35 @@
+package com.db.grad.javaapi.service;
+
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.repository.DogsRepository;
+import com.db.grad.javaapi.repository.DogsRepositoryStub;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DogsHandlerTest {
+
+    private DogsRepository itsDogRepo = new DogsRepositoryStub();
+
+    @BeforeEach
+    public void makeSureRepoIsEmpty() {
+        itsDogRepo.deleteAll();
+    }
+    @Test
+    public void add_a_dog_return_number_of_dogs_in_repo_is_one() {
+        // arange
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog( theDog );
+
+        int expectedResult = 1;
+
+        // act
+        long actualResult = cut.getNoOfDogs();
+
+        // assert
+        assertEquals( expectedResult, actualResult );
+    }
+}
