9695bd3a71c9c19607351edf6ab4d210f11cb893
vladp70 <vladpetrovici70@gmail.com>
2023-07-28 15:18:33 +0100

    Change data and schema to reflect Dogs Tdd.

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,17 +1,7 @@
 DROP TABLE IF EXISTS dogs;
-DROP TABLE IF EXISTS owner;
 
-create table owners
-(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    name varchar(30) NOT NULL
-);
-
-create table dogs
-(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    name varchar(30) NOT NULL,
-    age int,
-    owner_id int NOT NULL,
-    FOREIGN key(owner_id) REFERENCES owners (id)
+CREATE TABLE dogs (
+    dog_id INT NOT NULL,
+    name VARCHAR(250) NOT NULL,
+    age INT NOT NULL
 );
